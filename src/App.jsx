2d59be84e8c9f92bbcf63bbe08fdc84d0fb05752
Shane Kenzler <104663659+SKenzler@users.noreply.github.com>
2023-10-26 19:25:48 +0800

    re-install Tailwind for Vite

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import { Card } from "./components/Card";
 import { ThemeToggler } from "./components/ThemeToggler";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <hemeToggler />
+            <ThemeToggler />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
